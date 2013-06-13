e9e9bceff4570b07c7dd12ca856b8252afd5ad4c
John Morley <john.morley.93@gmail.com>
2013-06-13 17:28:08 +0100

    Changed all .lua files to make them compatible with Awesome 3.5.

--- obscur/theme.lua
+++ obscur/theme.lua
@@ -2,7 +2,8 @@
 -- licensed under the Creative Commons Attribution-Share (Alike License: http://creativecommons.org/licenses/by-sa/3.0/)
 
 --{{{ Main
-require("awful.util")
+local awful = require("awful")
+awful.util = require("awful.util")
 
 theme = {}
 
@@ -28,15 +29,15 @@ wallpaper4    = sharedthemes .. "/default/background.png"
 wpscript      = home .. "/.wallpaper"
 
 if awful.util.file_readable(wallpaper1) then
-	theme.wallpaper_cmd = { "awsetbg " .. wallpaper1 }
+	theme.wallpaper = wallpaper1
 elseif awful.util.file_readable(wallpaper2) then
-	theme.wallpaper_cmd = { "awsetbg " .. wallpaper2 }
+	theme.wallpaper = wallpaper2
 elseif awful.util.file_readable(wpscript) then
 	theme.wallpaper_cmd = { "sh " .. wpscript }
 elseif awful.util.file_readable(wallpaper3) then
-	theme.wallpaper_cmd = { "awsetbg " .. wallpaper3 }
+	theme.wallpaper = wallpaper3
 else
-	theme.wallpaper_cmd = { "awsetbg " .. wallpaper4 }
+	theme.wallpaper = wallpaper4
 end
 
 if awful.util.file_readable(config .. "/vain/init.lua") then
